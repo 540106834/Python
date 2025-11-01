b20b31e59b514785a91d1c1c5c1d215cd95fe145
540106834 <540106834@qq.com>
2025-11-01 22:59:50 +0900

    11

--- 阶段 1：Python 基础 + 运维脚本/python 练习程序/numbers.py
+++ 阶段 1：Python 基础 + 运维脚本/python 练习程序/numbers.py
@@ -1,5 +1,20 @@
+# 1. 定义两个变量 `a=15` 和 `b=4`，计算并打印：
+
+#    * 和
+#    * 差
+#    * 乘积
+#    * 浮点除法
+#    * 整除
+#    * 取余
+#    * 幂运算
+
 a=15
-b=14
+b=6
 
 print("a + b =", a + b)
 print("a - b =", a - b) 
+print("a * b =", a * b)
+print("a / b =", float(a / b))
+print("a // b =", a // b)
+print("a % b =", a % b)
+print("a ** b =", a ** b)
